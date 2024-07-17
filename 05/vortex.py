089e558c0de11abc96fe6e6141036ea9c3ba760d
Julian Karrer <juliankarrer@t-online.de>
2024-07-17 05:36:09 +0200

    Add energy spectrum to turbulence

--- 05/vortex.py
+++ 05/vortex.py
@@ -3,6 +3,7 @@ import numpy as np
 from muFFT import FFT
 import matplotlib.pyplot as plt
 from mpi4py import MPI
+import time
 
 
 # mpirun -np 4 python3 vortex.py
@@ -14,22 +15,35 @@ nx, ny, nz = nb_grid_pts
 lx, ly, lz = physical_sizes
 fft = FFT(nb_grid_pts, engine='mpi', communicator=MPI.COMM_WORLD)
 
-# test if MPI works, taken from muspectre docs
-if MPI.COMM_WORLD.rank == 0:
-    print('  Rank   Size          Domain       Subdomain        Location')
-    print('  ----   ----          ------       ---------        --------')
+# # test if MPI works, taken from muspectre docs
+# if MPI.COMM_WORLD.rank == 0:
+#     print('  Rank   Size          Domain       Subdomain        Location')
+#     print('  ----   ----          ------       ---------        --------')
 
-MPI.COMM_WORLD.Barrier()
-print(f'{MPI.COMM_WORLD.rank:6} {MPI.COMM_WORLD.size:6} {str(fft.nb_domain_grid_pts):>15} '
-      f'{str(fft.nb_subdomain_grid_pts):>15} {str(fft.subdomain_locations):>15}')
+# MPI.COMM_WORLD.Barrier()
+# print(f'{MPI.COMM_WORLD.rank:6} {MPI.COMM_WORLD.size:6} {str(fft.nb_domain_grid_pts):>15} '
+#       f'{str(fft.nb_subdomain_grid_pts):>15} {str(fft.subdomain_locations):>15}')
 
 # SETTINGS
 nu = 0.05
-dt = 0.001
-t_end = 30
+lam = 0.015
+t_end = 10
 
-info = r"$\nu="+str(nu)+r", \Delta t="+str(dt) + \
-    r", (x,y)\in[0; 2\pi ]^2, u_0 =\sin(x)\cos(y), v_0 =-\cos(x)\sin(y)$"
+# maximum timestep by CFL condition
+v_max = 1  # no velocity > 1 will be reached due to initial conditions and v decaying
+dt_max = min([lx/nx, ly/ny, lz/nz])/v_max
+dt = lam * dt_max
+print(dt_max, dt, lam)
+
+
+info = (
+    r"$\nu="+str(nu)
+    + r", \Delta t="+str(round(dt, 5))
+    + r", (x,y)\in[0; 2\pi ]^2$ on a $"
+    + str(nx)+r"\times "
+    + str(ny)+r"\times "
+    + str(nz)
+      + r"$ Grid, $u_0 =\sin(x)\cos(y), v_0 =-\cos(x)\sin(y)$")
 
 
 def curl(u_f, temp_f, curl_r):
@@ -120,6 +134,12 @@ def rk4(f, t: float, y: np.ndarray, dt: float) -> np.ndarray:
     return dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
 
+def rk2(f, _t: float, y: np.ndarray, dt: float) -> np.ndarray:
+    k1 = f(0, y)
+    k2 = f(0, y + dt / 2 * k1)
+    return dt * k2
+
+
 def analytic(t: float):
     x = np.array([fft.coords[0]*lx, fft.coords[1]*ly, fft.coords[2]*lz])
     sol = np.zeros(fft.coords.shape)
@@ -167,53 +187,181 @@ def plot_u(u, t, filename):
     fig.savefig(filename, dpi=400)
 
 
-if __name__ == "__main__":
-    # start the simulation
-    t = 0
-    xs = []
-    v_avg = []
-    v_ana = []
+def plot_v_avg_evolution(xs, v_avg, v_ana, filename):
+    # styling and setup
+    plt.style.use('default')
+    plt.rcParams['text.usetex'] = True
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.plot(xs, v_avg, "-r", label="measured velocities")
+    ax.plot(xs, v_ana, ":b", label="analytic solution")
+    ax.legend()
+    ax.set_title(
+        r"Time Evolution of the Average Velocity compared to Analytic Solution")
+    ax.set_xlabel(r"Time $t$")
+    ax.set_ylabel(r"Average Velocity $|\vec{\mathbf{v}}|_{avg}$")
+    fig.text(0.99, 0.01, info, horizontalalignment='right', fontsize="8")
+    fig.savefig(filename, dpi=400)
 
-    # plot the initial velocities (analytic solution at t=0)
+
+def print_and_record_v(v_avg, v_ana, xs, t, u):
     if MPI.COMM_WORLD.rank == 0:
-        plot_u(analytic(0), t, "vortex_initial.png")
+        xs += [t]
+        v_avg += [average_speed(u)]
+        v_ana += [average_speed(analytic(t))]
+        print(
+            "t={:.4f}".format(t),
+            "\t v_avg={:.10f}".format(v_avg[-1]),
+            "\t v_ana={:.10f}".format(v_ana[-1]),
+            "\t v_avg-v_ana={:.10f}".format(v_avg[-1]-v_ana[-1]),
+            end="\r"
+        )
 
-    u = analytic(0)
+
+def sim_explicit_euler(u=analytic(0)):
+    t = 0
+    xs: list[float] = []
+    v_avg: list[float] = []
+    v_ana: list[float] = []
     while t <= t_end:
         t += dt
         # update u
-        # u += rk4(du_dt, t, u, dt)
         u += du_dt(t, u) * dt
+        # print current state and record velocities
+        print_and_record_v(v_avg, v_ana, xs, t, u)
+    return xs, v_avg, v_ana
 
-        # print current state to stdout
-        if MPI.COMM_WORLD.rank == 0:
-            xs += [t]
-            v_avg += [average_speed(u)]
-            v_ana += [average_speed(analytic(t))]
-            print(
-                "t={:.4f}".format(t),
-                "\t v_avg={:.10f}".format(v_avg[-1]),
-                "\t v_ana={:.10f}".format(v_ana[-1]),
-                "\t v_avg-v_ana={:.10f}".format(v_avg[-1]-v_ana[-1]),
-                end="\r"
-            )
+
+def sim_rk4(u=analytic(0)):
+    t = 0
+    xs: list[float] = []
+    v_avg: list[float] = []
+    v_ana: list[float] = []
+    while t <= t_end:
+        t += dt
+        # update u
+        u += rk4(du_dt, t, u, dt)
+        # print current state and record velocities
+        print_and_record_v(v_avg, v_ana, xs, t, u)
+    return xs, v_avg, v_ana
+
+
+def sim_rk2(u=analytic(0)):
+    t = 0
+    xs: list[float] = []
+    v_avg: list[float] = []
+    v_ana: list[float] = []
+    while t <= t_end:
+        t += dt
+        # update u
+        u += rk2(du_dt, t, u, dt)
+        # print current state and record velocities
+        print_and_record_v(v_avg, v_ana, xs, t, u)
+    return xs, v_avg, v_ana
+
+
+def sim_adam_bash_2(u=analytic(0)):
+    t = 0
+    xs: list[float] = []
+    v_avg: list[float] = []
+    v_ana: list[float] = []
+
+    # use runge kutta for the initial step
+    du = rk4(du_dt, 0, u, dt)
+    prev_du_dt = du/dt
+    u += du
+    t += dt
+
+    while t <= t_end:
+        t += dt
+        # update u
+        du = du_dt(0, u)
+        u += dt/2*(3*du - prev_du_dt)
+        prev_du_dt = du
+        # print current state and record velocities
+        print_and_record_v(v_avg, v_ana, xs, t, u)
+    return xs, v_avg, v_ana
+
+
+def bench():
+    start = time.perf_counter()
+    xs_ab2, v_avg_ab2, v_ana_ab2 = sim_adam_bash_2()
+    time_ab2 = time.perf_counter()-start
+    if MPI.COMM_WORLD.rank == 0:
+        print("\nab2:", time_ab2)
+
+    start = time.perf_counter()
+    xs_exe, v_avg_exe, v_ana_exe = sim_explicit_euler()
+    time_exe = time.perf_counter()-start
+    if MPI.COMM_WORLD.rank == 0:
+        print("\nexe:", time_exe)
+
+    start = time.perf_counter()
+    xs_rk2, v_avg_rk2, v_ana_rk2 = sim_rk2()
+    time_rk2 = time.perf_counter()-start
+    if MPI.COMM_WORLD.rank == 0:
+        int(nz/2)
+    xs_rk4, v_avg_rk4, v_ana_rk4 = sim_rk4()
+    time_rk4 = time.perf_counter()-start
+    if MPI.COMM_WORLD.rank == 0:
+        print("\nrk4:", time_rk4)
 
     if MPI.COMM_WORLD.rank == 0:
-        print()
-
-        # show final velocity field
-        plot_u(u, t, "vortex_final.png")
-
-        # show time evolution of average speed
+        # first plot: accuracy
         plt.style.use('default')
         plt.rcParams['text.usetex'] = True
         fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-        ax.plot(xs, v_avg, "-r", label="measured velocities")
-        ax.plot(xs, v_ana, ":b", label="analytic solution")
+        ax.plot(xs_ab2, [abs(s-v) for v, s in zip(v_avg_ab2, v_ana_ab2)],
+                label="Adams-Bashforth 2")
+        ax.plot(xs_exe, [abs(s-v) for v, s in zip(v_avg_exe, v_ana_exe)],
+                label="Explicit Euler")
+        ax.plot(xs_rk2, [abs(s-v) for v, s in zip(v_avg_rk2, v_ana_rk2)],
+                label="Explicit Midpoint (RK2)")
+        ax.plot(xs_rk4, [abs(s-v) for v, s in zip(v_avg_rk4, v_ana_rk4)],
+                label="Runge Kutta 4")
         ax.legend()
         ax.set_title(
-            r"Time Evolution of the Average Velocity compared to Analytic Solution")
+            r"Time Evolution of Average Velocity Magnitude Error")
         ax.set_xlabel(r"Time $t$")
-        ax.set_ylabel(r"Average Velocity $|\vec{\mathbf{v}}|_{avg}$")
+        ax.set_ylabel(
+            r"Average Velocity Magnitude Error $v_{ana} - |\vec{\mathbf{v}}|_{avg}$")
+        ax.set_yscale('log')
         fig.text(0.99, 0.01, info, horizontalalignment='right', fontsize="8")
-        fig.savefig("average_v.png", dpi=400)
+        fig.savefig("bench_accuracy.png", dpi=300)
+
+        # second plot: accuracy vs computation time
+        fig2, ax2 = plt.subplots(1, 1, figsize=(8, 6))
+        ax2.scatter(
+            x=time_ab2, y=v_ana_ab2[-1] - v_avg_ab2[-1], label="Adams-Bashforth 2")
+        ax2.scatter(
+            x=time_exe, y=v_ana_exe[-1] - v_avg_exe[-1], label="Explicit Euler")
+        ax2.scatter(
+            x=time_rk2, y=v_ana_rk2[-1] - v_avg_rk2[-1], label="Explicit Midpoint (RK2)")
+        ax2.scatter(
+            x=time_rk4, y=v_ana_rk4[-1] - v_avg_rk4[-1], label="Runge Kutta 4")
+        ax2.legend()
+        ax2.set_title(
+            r"Simulation Accuracy vs. Computation Time")
+        ax2.set_xlabel(r"Computation Time (s)")
+        ax2.set_ylabel(
+            r"At $t="+str(t_end)+r"$: Average Velocity Magnitude Error $v_{ana}(t_{end}) - |\vec{\mathbf{v}}|_{avg}(t_{end})$")
+        fig2.text(0.99, 0.01, info, horizontalalignment='right', fontsize="8")
+        fig2.savefig("bench_time_vs_accuracy.png", dpi=300)
+
+
+if __name__ == "__main__":
+    # # benchmark explicit methods
+    # bench()
+
+    # plot the initial velocities(analytic solution at t=0)
+    if MPI.COMM_WORLD.rank == 0:
+        plot_u(u=analytic(0), t=0, filename="vortex_initial.png")
+
+    u = analytic(0)
+    xs, v_avg, v_ana = sim_adam_bash_2(u)
+
+    if MPI.COMM_WORLD.rank == 0:
+        print()
+        # show final velocity field
+        plot_u(u=u, t=t_end, filename="vortex_final.png")
+        # show time evolution of average speed
+        plot_v_avg_evolution(xs, v_avg, v_ana, "average_v.png")
--- 06/turbulence.py
+++ 06/turbulence.py
@@ -3,13 +3,13 @@ import numpy as np
 from muFFT import FFT
 import matplotlib.pyplot as plt
 from mpi4py import MPI
-from '../05/vortex.py' import *
+from scipy.stats import binned_statistic
 
 # mpirun -np 4 python3 vortex.py
 # 32x32x32 is fine
-n = 32
+n = 128
 nb_grid_pts = (n, n, n)
-physical_sizes = (2*np.pi, 2*np.pi, 2*np.pi)
+physical_sizes = (1, 1, 1)
 nx, ny, nz = nb_grid_pts
 lx, ly, lz = physical_sizes
 fft = FFT(nb_grid_pts, engine='mpi', communicator=MPI.COMM_WORLD)
@@ -26,21 +26,116 @@ print(f'{MPI.COMM_WORLD.rank:6} {MPI.COMM_WORLD.size:6} {str(fft.nb_domain_grid_
 # SETTINGS
 nu = 1/1600
 dt = 0.001
-t_end = 30
+t_end = 1
+velocity_amplitude = 1
 
 
-wavevector = (2 * np.pi * fft.fftfreq.T / grid_spacing).T
-zero_wavevector = (wavevector.T == np.zeros(3, dtype=int)).T.all(axis=0)
-wavevector_sq = np.sum(wavevector ** 2, axis=0)
+info = (
+    r"$\nu="+str(nu)
+    + r", \Delta t="+str(round(dt, 5))
+    + r", (x,y)\in[0; 1]^3$ on a $"
+    + str(nx)+r"\times "
+    + str(ny)+r"\times "
+    + str(nz)
+    + r"$ Grid, "
+    + r"$\\\vec{\mathbf{v}}_0\sim\mathcal{N}(0,1)$ modified such that $\nabla\cdot\vec{\mathbf{v}}_0=0, E_v(q)\propto q^{-\frac{5}{3}}$"
+)
+
+
+# modification: seed the rng to 42
+# CODE FROM https://pastewka.github.io/SpectralMethods/_project/milestone03.html START
+k = (2 * np.pi * fft.fftfreq.T / np.array(physical_sizes)).T
+k_eq_0_mask = (k.T == np.zeros(3, dtype=int)).T.all(axis=0)
+k_sq = np.sum(k ** 2, axis=0)
 # Fourier space velocity field
 random_field = np.zeros((3,) + fft.nb_fourier_grid_pts, dtype=complex)
-rng = np.random.default_rng()
+rng = np.random.default_rng(42)
 random_field.real = rng.standard_normal(random_field.shape)
 random_field.imag = rng.standard_normal(random_field.shape)
 # Initial velocity field should decay as k^(-5/3) for the Kolmogorov spectrum
-fac = np.zeros_like(wavevector_sq)
+fac = np.zeros_like(k_sq)
 # Avoid division by zero
-fac[np.logical_not(zero_wavevector)] = velocity_amplitude * \
-	wavevector_sq[np.logical_not(zero_wavevector)] ** (-5 / 6)
+fac[np.logical_not(k_eq_0_mask)] = velocity_amplitude * \
+    k_sq[np.logical_not(k_eq_0_mask)] ** (-5 / 6)
 random_field *= fac
+# CODE FROM https://pastewka.github.io/SpectralMethods/_project/milestone03.html END
 
+
+def plot_u(u, t, filename, slice=0):
+    print("Plotting ...")
+    print(u.shape)
+    x = np.array([fft.coords[0]*lx, fft.coords[1]*ly, fft.coords[2]*lz])
+    X, Y = np.meshgrid(x[0, :, 0, 0], x[1, 0, :, 0])
+    U, V = u[1, :, :, slice], u[0, :, :, slice]
+    C = np.linalg.norm(u[:, :, :, slice], axis=0)
+
+    # styling and setup
+    plt.style.use('dark_background')
+    plt.rcParams['text.usetex'] = True
+    fig, ax = plt.subplots(1, 1, figsize=(6, 8))
+    # plot description
+    ax.set_title(r"Stream Plot of Turbulent Flow at $t=" +
+                 "{:.1f}".format(t)+"$")
+    ax.set_xlabel(r"x")
+    ax.set_ylabel(r"y")
+    fig.text(0.99, 0.03, info, horizontalalignment='right', fontsize="8")
+    # plot it
+    _strm = ax.streamplot(
+        X, Y, U, V, linewidth=0.4, color=C, cmap='Spectral_r', density=9)
+    # create colourbar
+    sm = plt.cm.ScalarMappable(
+        cmap='Spectral_r', norm=plt.Normalize(vmin=C.min(), vmax=C.max()))
+    sm.set_array([])
+    cbar = fig.colorbar(sm, orientation="horizontal", pad=0.1, ax=ax)
+    cbar.set_label(r'Velocity Magnitude $|\vec{\mathbf{v}}|$')
+    # make axis pretty
+    ax.set_aspect('equal')
+    ax.margins(0)
+    # save to file
+    fig.savefig(filename, dpi=200)
+    print("Done Plotting!")
+
+
+def plot_power_spectrum(u):
+    k_mags = np.linalg.norm(k, axis=0)**2
+    u_sq = np.linalg.norm(u, axis=0)**2
+    e = u_sq*0.5
+
+    x_min = max(np.min(k_mags), 1/nb_grid_pts[0])
+    x_max = np.max(k_mags)
+    xs = np.linspace(x_min, x_max, 100)
+    bin_means, bin_edges, _ = binned_statistic(
+        x=k_mags.flatten(),
+        values=e.flatten(),
+        bins=np.logspace(np.log10(x_min), np.log10(x_max), 50)
+    )
+    bin_width = (bin_edges[1] - bin_edges[0])
+    bin_centers = bin_edges[1:] - bin_width/2
+
+    plt.loglog(bin_centers, bin_means,  "-r")
+
+    plt.plot(xs, xs**(-5/3), "-g")
+    plt.show()
+
+
+# define fields
+u_r = fft.real_space_field('u', (3,))
+u_f = fft.fourier_space_field('u_hat', (3,))
+
+
+print("divergence before:", np.abs(np.mean(random_field * k)))
+
+# make the random_field divergence free
+k_k_tensor_prod = np.einsum('i...,j...->ij...', k, k)
+k_tensor_k_over_k_sq = k_k_tensor_prod / np.where(k_sq == 0, 1, k_sq)
+id = np.eye(3)[:, :, np.newaxis, np.newaxis, np.newaxis]
+project_div_free = id - k_tensor_k_over_k_sq
+u_f.p = np.einsum('ij...,j...->i...', project_div_free, random_field)
+
+print("divergence after:", np.abs(np.mean(u_f.p * k)))
+
+fft.ifft(u_f, u_r)
+
+if MPI.COMM_WORLD.rank == 0:
+    plot_power_spectrum(u_f.p)
+    # plot_u(u_r.p, 0, "rand.png", 0)
